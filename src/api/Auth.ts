2c340b9d33d760721ada18d450c9e9764ca57521
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2019-09-29 16:07:37 -0300

    Fixing register url and adding geById

--- src/api/Auth.ts
+++ src/api/Auth.ts
@@ -1,40 +1,49 @@
 import httpClient from 'http-utils';
 
 interface LoginData {
-  username: string;
-  password: string;
+	username: string;
+	password: string;
 }
 
 interface RegisterData {
-  username: string;
-  email: string;
-  password: string;
+	username: string;
+	email: string;
+	password: string;
 }
 
 export default class Auth {
-  static login(data: LoginData) {
-    return httpClient
-      .post('/token', data)
-      .then(response => {
-        return response;
-      })
-      .catch(error => {
-        throw error;
-      });
-  }
+	static login(data: LoginData) {
+		return httpClient
+			.post('/token', data)
+			.then(response => {
+				return response;
+			})
+			.catch(error => {
+				throw error;
+			});
+	}
 
-  static register(data: RegisterData) {
-    return httpClient
-      .post('/register', data)
-      .then(response => {
-        return response;
-      })
-      .catch(error => {
-        throw error;
-      })
-  }
+	static register(data: RegisterData) {
+		return httpClient
+			.post('/profiles/simple/', data)
+			.then(response => {
+				return response;
+			})
+			.catch(error => {
+				throw error;
+			});
+	}
 
-  static delete() {}
+	static delete() {}
 
-  static getById() {}
+	static getById(id: number) {
+		return httpClient
+			.get(`/profiles/simple/${id}/`)
+			.then(response => {
+				return response;
+			})
+			.catch(error => {
+				throw error;
+			});
+	}
 }
