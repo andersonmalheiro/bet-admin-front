7b3d187787da999efa6d33bbd243ae0f13964da4
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2019-09-29 16:08:14 -0300

    Updating admin page

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import './style.scss';
 import logo from 'assets/img/logo-home.png';
 import Sidebar from 'components/Sidebar';
-
-import { Route, BrowserRouter, Link, Redirect } from 'react-router-dom';
 import Dashboard from 'pages/Dashboard';
+import { Route, BrowserRouter, Link, Redirect } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
+import Auth from 'api/Auth';
 
 export default function Admin({ match }) {
 	const [expanded, setExpanded] = useState(true);
+	const [user, setUser] = useState('');
 	console.log(match);
 	function toggleExpanded() {
 		setExpanded(!expanded);
@@ -18,14 +20,33 @@ export default function Admin({ match }) {
 		window.location.href = '/';
 	}
 
+	function getUser() {
+		const token = localStorage.getItem('access');
+		if (token) {
+			const { user_id } = jwtDecode(token);
+			if (user_id) {
+				Auth.getById(user_id)
+					.then(response => {
+						localStorage.setItem('user', JSON.stringify(response.data));
+						setUser(response.data.first_name);
+					})
+					.catch(err => {
+						console.error(err);
+					});
+			}
+		}
+	}
+
 	document.title = 'BetAdmin';
+	getUser();
+
 	return (
 		<BrowserRouter>
 			<div className="wrapper">
 				<Sidebar
 					city_name="Juazeiro do Norte"
 					company_logo="https://abrilexame.files.wordpress.com/2018/10/mcdonalds-pinheiros.jpg"
-					company_name="McDonalds"
+					company_name={user}
 					match={match}
 					expanded={expanded}
 					id={1}
